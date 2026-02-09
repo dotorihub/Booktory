980dfeb7132b1f852798483a8f26247cbaaa7d17
JiHeyonMon <jhk8806@naver.com>
2026-02-09 20:56:00 +0900

    first commit

---
+++ Booktory/BooktoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooktoryApp.swift
+//  Booktory
+//
+//  Created by 김지현 on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BooktoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
