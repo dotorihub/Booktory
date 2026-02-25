69639950270fc020f22c64aa8c63b2402c6b619e
JiHeyonMon <jhk8806@naver.com>
2026-02-25 20:11:55 +0900

    fix: bundle ID 변경, 최소 지원 OS 변경

---
+++ Booktory/UI/Timer/BGTimer/LiveTimerWidget.swift
@@ -0,0 +1,8 @@
+//
+//  LiveTimerWidget.swift
+//  Booktory
+//
+//  Created by 김지현 on 2/11/26.
+//
+
+import Foundation
---
+++ Booktory/UI/Timer/BGTimer/TimerActivityAttributes.swift
@@ -0,0 +1,8 @@
+//
+//  TimerActivityAttributes.swift
+//  Booktory
+//
+//  Created by 김지현 on 2/11/26.
+//
+
+import Foundation
